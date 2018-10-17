764c1c83b9b3a4c8a9900e2b0be6823d8c4192fc
byronmackay <bqmackay@gmail.com>
2018-10-17 11:14:58 -0600

    Update network provider to accept path and extra params

--- src/providers/network-provider.ts
+++ src/providers/network-provider.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 
 export class NetworkProvider {
 
-  baseUrl: string = 'http://api.openweathermap.org/data/2.5/weather?APPID=985fd02cf5bd424f5c6e2b61f7397987&units=imperial&';
+  baseUrl: string = 'http://api.openweathermap.org/data/2.5/'
+  api_params: string = '?APPID=985fd02cf5bd424f5c6e2b61f7397987&units=imperial&';
 
   constructor(public http: HttpClient) {}
 
-  get(path) {
-    const fullPath = this.baseUrl + path;
+  get(path, params) {
+    const fullPath = this.baseUrl + path + this.api_params + params;
     return this.http.get(fullPath, { observe: 'response' });
   }
 
