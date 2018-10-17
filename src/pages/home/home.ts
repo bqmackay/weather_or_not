d60ae9c78765103a045fb1bec76585718c847fae
byronmackay <bqmackay@gmail.com>
2018-10-17 16:14:48 -0600

    Moved code around to clean it up

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,59 +13,14 @@ export class HomePage {
   fiveDayWeather: object;
 
   constructor(public navCtrl: NavController, public weatherProvider: WeatherProvider) {
-    if (localStorage["now"] == null) {
-      var response = this.weatherProvider.getCityWeather('Provo')
-      response.subscribe(data => {
-        console.log(data);
-        localStorage["now"] = JSON.stringify(data.body);
-        this.now = data.body;
-        this.now.weather[0].icon = "http://openweathermap.org/img/w/" + this.now.weather[0].icon + ".png";
-      }, error => {
-        console.log("No weather returned");
-      })
-    } else {
-      this.now = JSON.parse(localStorage["now"]);
-      this.now.weather[0].icon = "http://openweathermap.org/img/w/" + this.now.weather[0].icon + ".png";
-      this.now.dt_txt = new Date();
-      console.log(this.now);
-    }
+    this.weatherProvider.getCityWeather('Provo').then(data => {
+      this.now = data;
+    })
 
-    if (localStorage["forecast"] == null) {
-      var weekResponse = this.weatherProvider.getFiveDayWeather('Provo')
-      weekResponse.subscribe(data => {
-        console.log(data);
-        localStorage["forecast"] = JSON.stringify(data.body.list);
-        this.fiveDayWeather = data.body.list;
-      }, error => {
-        console.log("No 5 day forcast returned");
-      })
-    } else {
-      this.fiveDayWeather = JSON.parse(localStorage["forecast"])
-      console.log(this.fiveDayWeather);
-    }
-
-    //Processing Date Info
-    for (let day of this.fiveDayWeather) {
-      var date = new Date(day.dt_txt);
-      var month = date.getMonth() + 1;
-      var dayOfMonth = date.getDate();
-      var hour = date.getHours();
-      var isMorning = hour < 12;
-      debugger
-      if (hour > 12) {
-        hour = hour - 12;
-      }
-      if (hour == 0) {
-        hour = 12;
-      }
-      day.dt_txt = hour + (isMorning ? "AM" : "PM");
-
-      //format temp
-      day.main.temp = parseInt(day.main.temp);
-
-      day.weather[0].icon = "http://openweathermap.org/img/w/" + day.weather[0].icon + ".png";
-    }
-
+    this.weatherProvider.getFiveDayWeather('Provo').then(data => {
+      this.fiveDayWeather = data;
+    })
+    
   }
 
 
--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -16,11 +16,66 @@ export class WeatherProvider extends NetworkProvider {
   }
 
   getCityWeather(city_name) {
-    return this.get('weather', 'q=' + city_name);
+    return new Promise(resolve => {
+      if (localStorage["now"] != null) {
+        var now = JSON.parse(localStorage["now"]);
+        now = this.formatWeatherObject(now);
+        resolve(now);
+      } else {
+        this.get('weather', 'q=' + city_name).subscribe(data => {
+          var now = data.body
+          localStorage["now"] = JSON.stringify(now);
+          now = this.formatWeatherObject(now);
+          resolve(now);
+        }, error => {
+          console.log("No weather returned");
+        })
+      }
+    })
   }
 
   getFiveDayWeather(city_name) {
-    return this.get('forecast', 'q=' + city_name);
+    return new Promise(resolve => {
+      if (localStorage["forecast"] == null) {
+        this.get('forecast', 'q=' + city_name).subscribe(data => {
+          var forecast = this.processForecastData(data.body.list);
+          localStorage["forecast"] = JSON.stringify(forecast);
+          resolve(forecast);
+        }, error => {
+          console.log("No 5 day forcast returned");
+        })
+      } else {
+        resolve(JSON.parse(localStorage["forecast"]))
+      }
+    })
+  }
+
+  formatWeatherObject(weatherObject) {
+    weatherObject.weather[0].icon = "http://openweathermap.org/img/w/" + weatherObject.weather[0].icon + ".png";
+    weatherObject.dt_txt = new Date();
+    return weatherObject;
+  }
+
+  processForecastData(data) {
+    for (let day of data) {
+      var date = new Date(day.dt_txt);
+      var hour = date.getHours();
+      var isMorning = hour < 12;
+
+      if (hour > 12) {
+        hour = hour - 12;
+      }
+      if (hour == 0) {
+        hour = 12;
+      }
+      day.dt_txt = hour + (isMorning ? "AM" : "PM");
+
+      //format temp
+      day.main.temp = parseInt(day.main.temp);
+
+      day.weather[0].icon = "http://openweathermap.org/img/w/" + day.weather[0].icon + ".png";
+    }
+    return data;
   }
 
 }
